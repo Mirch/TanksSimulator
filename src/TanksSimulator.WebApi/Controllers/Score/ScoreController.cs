711c13569ea5fc3d72e40916279e414fff57f8b3
Mircea Teodor Oprea <mirceateodor.oprea@gmail.com>
2020-07-13 21:56:49 +0300

    Add score controller

---
+++ src/TanksSimulator.WebApi/Controllers/Score/ScoreController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TanksSimulator.WebApi.Data;
+
+namespace TanksSimulator.WebApi.Controllers.Score
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScoreController : ControllerBase
+    {
+        private readonly GameDataRepository _gameDataRepository;
+
+        public ScoreController(
+            GameDataRepository gameDataRepository)
+        {
+            _gameDataRepository = gameDataRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _gameDataRepository
+                .GetAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAll(string id)
+        {
+            var result = await _gameDataRepository
+                .GetByIdAsync(id);
+
+            if (result == null)
+            {
+                return NotFound(new { error = "Could not find any data for this battle." });
+            }
+
+            return Ok(result);
+        }
+
+    }
+}
